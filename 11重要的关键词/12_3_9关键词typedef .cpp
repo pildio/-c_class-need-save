2262e7ce5f6577e456f53004f20117a0ef1ec775
pildio <2131698788@qq.com>
2022-03-09 10:27:01 +0800

    2022年3月9日10:26:54上传代码

---
+++ 11重要的关键词/12_3_9关键词typedef .cpp
@@ -0,0 +1,12 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//typedef unsigned int u_int;// typedef的意思是给过长的关键词自定义 
+////被重定义的类型并不是一个新的类型，仅仅只是原有的类型取了一个新的名字
+//int main()
+//{
+//    unsigned int num1 = 0;
+//    u_int num2 = 0;    //观察num1和num2,这两个变量的类型是一样的
+//    printf("%d\n", sizeof(num1));
+//    printf("%d\n", sizeof(num2));
+//    return 0;
+//}
