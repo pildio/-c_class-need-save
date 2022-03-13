dec021bab566397628a416a990263db361beb087
pildio <2131698788@qq.com>
2022-03-13 21:19:05 +0800

    2022年3月13日21:18:58提交代码

---
+++ 15 分支语句和循环语句/19_3_13 if语句练习.cpp
@@ -0,0 +1,63 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+////1.要求 在控制台输入一组年龄 0-18未成年，18-26青年，26-40中年，40-60壮年，60-100老年
+
+//#include<stdio.h>
+//int main()
+//{
+//	int age = 0;
+//	printf("输入你的年龄_>");
+//	scanf("%d", &age);
+//	if (age < 18)
+//		printf("未成年\n");
+//	else if (age >= 18 && age < 26)
+//		printf("青年\n");
+//	else if (age >= 26 && age < 40)
+//		printf("中年\n");
+//	else if (age >= 40 && age < 60)
+//		printf("壮年\n");
+//	else if (age >= 60 && age < 100)
+//		printf("老年\n"); 
+//	else
+//		printf("老不死\n");
+//		return 0;
+//}
+//
+//
+//
+//
+
+////2. 判断一个数是否为奇数
+
+//#include<stdio.h>
+//int main()
+//{
+//	int num = 0;
+//	printf("请输入一个数\n");
+//	scanf("%d", &num);
+//	if (num % 2 == 1)
+//	{
+//		printf("是奇数\n");
+//	}
+//	else
+//		{
+//		printf("不是奇数\n");
+//		}
+//		return 0;
+//}
+
+////3.输出1-100之间的奇数
+
+//#include<stdio.h>
+//int  main()
+//{
+//	int i = 0;
+//	while (i < 100)
+//	{
+//		i++;
+//		if(i%2==1)
+//		{ 
+//		printf("%d\t", i);
+//		}
+//	}
+//	return 0;
+//}
